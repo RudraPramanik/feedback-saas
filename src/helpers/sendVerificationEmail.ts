4fa2ff3da19fc91bc00a9b274bdc273fe4ea3bff
Rudra <rudrahimangshoo2020@gmail.com>
2024-07-10 17:47:18 +0600

    send-verification-code

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -3,20 +3,20 @@ import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from '@/types/ApiResponse';
 
 export async function sendVerificationEmail(
-  email: string,
-  username: string,
-  verifyCode: string
-): Promise<ApiResponse> {
+  email:string,
+  username:string,
+  verifyCode:string
+):Promise<ApiResponse>{
   try {
     await resend.emails.send({
-      from: 'dev@hiteshchoudhary.com',
-      to: email,
-      subject: 'Mystery Message Verification Code',
-      react: VerificationEmail({ username, otp: verifyCode }),
-    });
+      from: 'rudrahimangshoo2020@gmail.com',
+      to:email,
+      subject:"Mystery Message Verification Code",
+      react: VerificationEmail({username, otp:verifyCode}),
+    })
     return { success: true, message: 'Verification email sent successfully.' };
   } catch (emailError) {
-    console.error('Error sending verification email:', emailError);
+    console.error('Error sending verification email:',emailError);
     return { success: false, message: 'Failed to send verification email.' };
   }
 }
