9889823673e60c1831e72c1feebc014cbb3ead26
Rudra <rudrahimangshoo2020@gmail.com>
2024-07-10 17:06:31 +0600

    update-credential

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -7,25 +7,28 @@ type ConnectionObject = {
 const connection: ConnectionObject = {};
 
 async function dbConnect(): Promise<void> {
-  // Check if we have a connection to the database or if it's currently connecting(there can be multiple db)
   if (connection.isConnected) {
     console.log('Already connected to database');
     return;
   }
 
   try {
-    // Attempt to connect to the database
     const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
 
     connection.isConnected = db.connections[0].readyState;
 
-    console.log('Database is connected successfully');
+    console.log('db is connected successfully')
+  
   } catch (error) {
     console.error('Database connection failed:', error);
 
-    // Graceful exit in case of a connection error
     process.exit(1);
   }
 }
 
 export default dbConnect;
+
+  // Check if we have a connection to the database or if it's currently connecting(there can be multiple db)
+      // Attempt to connect to the database
+    // console.log(db.connection,'Database is connected successfully');
+    // Graceful exit in case of a connection error
